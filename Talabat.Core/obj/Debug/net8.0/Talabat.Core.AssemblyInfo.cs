c7e8a7ba76ce695e2a671d81e1c0fa044751a52c
MahmoudAmin <ma7moud888.amim@gmail.com>
2025-02-28 09:21:34 +0200

    Adding Products Domain Models

--- Talabat.Core/obj/Debug/net8.0/Talabat.Core.AssemblyInfo.cs
+++ Talabat.Core/obj/Debug/net8.0/Talabat.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039bcff48bb19e649c98e8c38496dbded086f3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
