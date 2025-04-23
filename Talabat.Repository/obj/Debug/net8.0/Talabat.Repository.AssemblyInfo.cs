9dd013457d84b1216a5e6b44a578577db1e02bfb
MahmoudAmin <ma7moud888.amim@gmail.com>
2025-04-23 13:17:11 +0200

    Login EndPoint Created

--- Talabat.Repository/obj/Debug/net8.0/Talabat.Repository.AssemblyInfo.cs
+++ Talabat.Repository/obj/Debug/net8.0/Talabat.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d22a729e756261b71c505bbb7f7085037793f21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5edd40f89b63a3a71ddac6c27e6c682ec33c4c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
