876f42b0566f9e50a908eec719f1195b39e55f3f
Mahmoud Amin <139350214+MahmoudAmin5@users.noreply.github.com>
2025-06-27 15:19:36 +0300

    Oreder Service Interface created

---
+++ Talabat.Core/Services.Core/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.OrderAggregate;
+
+namespace Talabat.Core.Services.Core
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(string BuyerEmail, string BasketId, int DeliveryMethodId, Address ShippingAddress);
+        Task<Order> GetOrdersForSpecificUserAsync(string BuyerEmail);
+        Task<Order> GetOrdersByIdForSpecificUserAsync(string BuyerEmail, int OrderId);
+
+    }
+}
--- Talabat.Repository/obj/Debug/net8.0/Talabat.Repository.AssemblyInfo.cs
+++ Talabat.Repository/obj/Debug/net8.0/Talabat.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d891416187104df115fa8b15afaf78ce3389970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25da8bfde0b4c81063b6f1a00529fcdaa241f88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
