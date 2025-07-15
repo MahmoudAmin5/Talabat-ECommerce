9aa1f9b15ac46f5b2ddb684b64e4accf02d604b5
Mahmoud Amin <139350214+MahmoudAmin5@users.noreply.github.com>
2025-07-15 13:56:18 +0300

    Order To Return Dto and OrderItemDto Created

---
+++ Talabat.APIs/DTO/OrderItemDto.cs
@@ -0,0 +1,13 @@
+﻿using Talabat.Core.OrderAggregate;
+
+namespace Talabat.APIs.DTO
+{
+    public class OrderItemDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureURL { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
